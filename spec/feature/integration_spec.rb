e32c92389ce53d39f6be980bec9c68a9854756be
Jose Carrillo <34354911+jose01carrillo30@users.noreply.github.com>
2022-09-14 15:56:35 -0500

    added tests for new fields

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rolling'
+    fill_in 'Publication Date', with: '7/8/09'
+    fill_in 'Price', with: '90.01'
+
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
