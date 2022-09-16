15a7f2bec8e8c05afcbf9185fb40827abcbfc2eb
Jose Carrillo <34354911+jose01carrillo30@users.noreply.github.com>
2022-09-15 22:37:39 -0500

    fixed bug in unit test

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -14,51 +14,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
-end
 
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter', author: 'jk rolling', pub_date: '01/02/2000', price: '10.99')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
+  it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
   end
-end
 
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter', author: 'jk rolling', pub_date: '01/02/2000', price: '10.99')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
+  it 'is not valid without a date' do
     subject.pub_date = nil
     expect(subject).not_to be_valid
   end
-end
 
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(title: 'harry potter', author: 'jk rolling', pub_date: '01/02/2000', price: '10.99')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
+  it 'is not valid without a price' do
     subject.price = nil
     expect(subject).not_to be_valid
   end
+
 end
